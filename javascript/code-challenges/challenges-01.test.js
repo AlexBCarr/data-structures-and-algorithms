4d8cc59968754977b5779c5012788ff91c055dfd
Alex B Carr <mralex.carr@yahoo.com>
2023-03-23 15:44:48 -0700

    Code Updates

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -41,9 +41,11 @@ Use `forEach` to loop over the input array. The modified strings should each be 
 
 const allUpperCase = (arr) => {
   // Solution code here...
-  let newArr = [];
-  arr.forEach(letters => newArr.push.toUpperCase(letters));
-  return newArr;
+  let result = [];
+  arr.forEach((str) => {
+    result.push(str.toUpperCase());
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -58,12 +60,17 @@ Use `forEach` to build a new array of strings, each string modified by the callb
 
 const greeting = (word) => {
   // Solution code here...
-  let newArr = [];
-  word.forEach(string => newArr.push.toUpperCase(string + '!'));
-  return newArr;
+  return `${word.toUpperCase()}!`;
 };
 
 const speaker = (words, callback) => {
+  // Solution code here...
+  let result = [];
+  words.forEach((str)=>{
+    result.push(callback(str));
+  });
+  return result;
+
   // Solution code here...
 };
 
@@ -85,10 +92,15 @@ Return the modified array.
 
 const addValues = (arr, value) => {
   // Solution code here...
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+  for (let i = 0; i < times; i++) {
+    callback(arr, num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -111,6 +123,13 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  let groceryList = [];
+  availableItems.forEach((item)=> {
+    if (item.available) {
+      groceryList.push(item.name);
+    }
+  });
+  return groceryList;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -129,6 +148,7 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
